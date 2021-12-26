8cc5f51be60828aff51da5ee497c186b9a0cbb06
Yogesh kumar <yk6130814@gmail.com>
2021-12-26 19:12:34 +0530

    final commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,6 @@ import Men from "../../images/logo.png";
 import Typed from "typed.js";
 import { useEffect } from "react";
 import Tilt from "react-tilt";
-import Particles from "react-tsparticles";
 
 export default function Hero() {
   useEffect(() => {
@@ -17,96 +16,10 @@ export default function Hero() {
       typed.destroy();
     };
   }, []);
-  const particlesInit = (main) => {
-    console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <section className="home" id="home">
-      <div className="particle">
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            fpsLimit: 60,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                bubble: {
-                  distance: 400,
-                  duration: 2,
-                  opacity: 0.8,
-                  size: 40,
-                },
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#e38528",
-              },
-              links: {
-                color: "#ff5421",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outMode: "bounce",
-                random: false,
-                speed: 3,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                random: true,
-                value: 5,
-              },
-            },
-            detectRetina: true,
-          }}
-        />
-      </div>
+    
       <div className="content">
         <Tilt>
           <img className="tilt" src={Men} alt="" />
@@ -142,7 +55,7 @@ export default function Hero() {
             <i className="fab fa-instagram"></i>
           </a>
           <a
-            href="https://github.com/yogesh-kumar-27"
+            href="https://www.linkedin.com/in/yogesh-kumar-772b11228/"
             rel="noreferrer"
             target="_blank"
           >
